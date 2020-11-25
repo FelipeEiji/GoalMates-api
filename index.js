5cdc3a4e7ee74434ee6e650800cde157c2303be4
Felipe Maruyama <felipe-eiji@hotmail.com>
2020-11-25 00:43:54 -0300

    fix error status

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const { ApolloServer } = require("apollo-server");
+const {
+  HttpQueryError,
+  ApolloError,
+  AuthenticationError,
+} = require("apollo-server-core");
 const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
+  formatResponse(body) {
+    if (
+      body.errors &&
+      body.errors.find(
+        (err) => err.extensions && err.extensions.code === "UNAUTHENTICATED"
+      )
+    ) {
+      return {
+        ...body,
+        data: undefined,
+      };
+    }
+
+    return body;
+  },
+
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req }),
+  plugins: [
+    {
+      requestDidStart() {
+        return {
+          didEncounterErrors({ response, errors }) {
+            if (
+              errors.find(
+                (err) =>
+                  err instanceof AuthenticationError ||
+                  err.originalError instanceof AuthenticationError
+              )
+            ) {
+              response.data = undefined;
+              response.http.status = 401;
+            }
+          },
+        };
+      },
+    },
+  ],
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true})
+  .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB Connected");
     return server.listen({ port: PORT });
@@ -22,6 +63,6 @@ mongoose
   .then((res) => {
     console.log(`Server running at ${res.url}`);
   })
-  .catch(err =>{
+  .catch((err) => {
     console.error(err);
-  })
+  });
